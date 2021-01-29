3d395ab307fb4a3305c98bbbeb80677b60ac4252
Martin Najjar <martin_najjar@yahoo.com>
2021-01-29 23:11:29 +0300

    Make the ship stop when hit by another object

--- src/scenes/SceneMain.js
+++ src/scenes/SceneMain.js
@@ -40,52 +40,166 @@ export default class SceneMain extends Phaser.Scene {
     this.enemies = this.add.group();
     this.enemyLasers = this.add.group();
     this.playerLasers = this.add.group();
-
-    this.time.addEvent({
-      delay: 3000,
-      callback: function () {
-        var enemy = new GunShip(
-          this,
-          Phaser.Math.Between(0, this.game.config.width),
-          0,
-        );
-        this.enemies.add(enemy);
-      },
-      callbackScope: this,
-      loop: true,
-    });
-
     this.player = new Player(
       this,
       this.game.config.width * 0.5,
       this.game.config.height * 0.5,
       'ship',
     );
+
+    this.time.addEvent({
+      delay: 3000,
+      callback: function () {
+        let enemy = null;
+
+        if (Phaser.Math.Between(0, 10) >= 3) {
+          enemy = new GunShip(
+            this,
+            Phaser.Math.Between(0, this.game.config.width),
+            0,
+          );
+        }
+        // else if (Phaser.Math.Between(0, 10) >= 5) {
+        //   if (this.getEnemiesByType('ChaserShip').length < 5) {
+        //     enemy = new ChaserShip(
+        //       this,
+        //       Phaser.Math.Between(0, this.game.config.width),
+        //       0,
+        //     );
+        //   }
+        // } else {
+        //   enemy = new CarrierShip(
+        //     this,
+        //     Phaser.Math.Between(0, this.game.config.width),
+        //     0,
+        //   );
+        // }
+
+        if (enemy !== null) {
+          enemy.setScale(Phaser.Math.Between(10, 20) * 0.1);
+          this.enemies.add(enemy);
+        }
+      },
+      callbackScope: this,
+      loop: true,
+    });
+
+    this.physics.add.collider(
+      this.playerLasers,
+      this.enemies,
+      function (playerLaser, enemy) {
+        if (enemy) {
+          if (enemy.onDestroy !== undefined) {
+            enemy.onDestroy();
+          }
+
+          enemy.explode(true);
+          playerLaser.destroy();
+        }
+      },
+    );
+
+    this.physics.add.overlap(
+      this.player,
+      this.enemies,
+      function (player, enemy) {
+        if (!player.getData('isDead') && !enemy.getData('isDead')) {
+          player.explode(false);
+          enemy.explode(true);
+        }
+      },
+    );
+
+    this.physics.add.overlap(
+      this.player,
+      this.enemyLasers,
+      function (player, laser) {
+        if (!player.getData('isDead') && !laser.getData('isDead')) {
+          player.explode(false);
+          laser.destroy();
+        }
+      },
+    );
   }
 
   update() {
     this.player.update();
 
-    if (this.cursors.up.isDown) {
-      this.player.moveUp();
-    } else if (this.cursors.down.isDown) {
-      this.player.moveDown();
+    if (!this.player.getData('isDead')) {
+      this.player.update();
+      if (this.cursors.up.isDown) {
+        this.player.moveUp();
+      } else if (this.cursors.down.isDown) {
+        this.player.moveDown();
+      }
+      if (this.cursors.left.isDown) {
+        this.player.moveLeft();
+      } else if (this.cursors.right.isDown) {
+        this.player.moveRight();
+      }
+
+      if (this.keySpace.isDown) {
+        this.player.setData('isShooting', true);
+      } else {
+        this.player.setData(
+          'timerShootTick',
+          this.player.getData('timerShootDelay') - 1,
+        );
+        this.player.setData('isShooting', false);
+      }
     }
 
-    if (this.cursors.left.isDown) {
-      this.player.moveLeft();
-    } else if (this.cursors.right.isDown) {
-      this.player.moveRight();
+    for (let enemy of this.enemies.getChildren()) {
+      enemy.update();
+      if (
+        enemy.x < -enemy.displayWidth ||
+        enemy.x > this.game.config.width + enemy.displayWidth ||
+        enemy.y < -enemy.displayHeight * 4 ||
+        enemy.y > this.game.config.height + enemy.displayHeight
+      ) {
+        if (enemy) {
+          if (enemy.onDestroy !== undefined) {
+            enemy.onDestroy();
+          }
+
+          enemy.destroy();
+        }
+      }
     }
 
-    if (this.keySpace.isDown) {
-      this.player.setData('isShooting', true);
-    } else {
-      this.player.setData(
-        'timerShootTick',
-        this.player.getData('timerShootDelay') - 1,
-      );
-      this.player.setData('isShooting', false);
+    for (let laser of this.enemyLasers.getChildren()) {
+      laser.update();
+      if (
+        laser.x < -laser.displayWidth ||
+        laser.x > this.game.config.width + laser.displayWidth ||
+        laser.y < -laser.displayHeight * 4 ||
+        laser.y > this.game.config.height + laser.displayHeight
+      ) {
+        if (laser) {
+          laser.destroy();
+        }
+      }
     }
+
+    for (let laser of this.playerLasers.getChildren()) {
+      laser.update();
+      if (
+        laser.x < -laser.displayWidth ||
+        laser.x > this.game.config.width + laser.displayWidth ||
+        laser.y < -laser.displayHeight * 4 ||
+        laser.y > this.game.config.height + laser.displayHeight
+      ) {
+        if (laser) {
+          laser.destroy();
+        }
+      }
+    }
+  }
+
+  getEnemiesByType(type) {
+    const enemiesArray = this.enemies
+      .getChildren()
+      .filter(enemy => enemy.getData('type') == type);
+    return enemiesArray;
   }
 }
